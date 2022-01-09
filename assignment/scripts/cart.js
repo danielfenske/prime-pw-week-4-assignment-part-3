9a54d83f5ef93ec817ad2b150844e3b6ec51aa0f
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-01-09 11:41:24 -0600

    Modified (updated) and tested addItem function for stretch goal #3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,6 +2,8 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
+const maxItems = 5;
+
 // 1. Create a global variable named 'basket' and set it to an empty array.
 
 let basket = [];
@@ -14,18 +16,22 @@ console.log(basket); //Statement output: [] (empty array)
   // Add the new item to the global array 'basket'
   // Return TRUE indiciating the item was added
 
-function addItem(item) {
-  console.log('in addItem');
-  basket.push(item); // adds item to basket array
-  return true; //verifies item was added to basket
-} // end addItem
+  function addItem(item) {
+    console.log('in addItem');
+    if (isFull()){
+      return false; // Will not allow item to be added to basket if # of items total more than maxItems
+    } else {
+      basket.push(item);  // adds item to basket array
+      return true; //verifies item was added to basket
+    }
+  } // end addItem
 
 
 // TESTING basket function
 addItem('bread');
-console.log(basket);
+console.log('What is in the basket?', basket);
 addItem('berries');
-console.log(basket); //Statement output: ['bread', 'berries']
+console.log('What is in the basket?', basket); //Statement output: ['bread', 'berries']
 
 
 
@@ -66,8 +72,8 @@ console.log(basket); // Statement output: [] (basket is now empty)
 
 // 1. Add a global const named 'maxItems' and set it to 5.
 
-const maxItems = 5;
-console.log(maxItems); // Statement output: 5
+//NOTE: const 'maxItems' was created on line 5 for proprer initialization within addItem function.
+console.log('Max items are set to', maxItems); // Statement output: 5
 
 
 
@@ -78,14 +84,17 @@ console.log(maxItems); // Statement output: 5
 function isFull() {
   console.log('in isFull');
   if (basket.length < maxItems) {
+    console.log('The basket is not full');
     return false;
   } else {
+    console.log('The basket is full');
     return true;
   }
 } // end isFulls
 
 
 // Adding items to basket array
+console.log(basket);
 addItem('milk');
 addItem('veggies');
 addItem('bacon');
@@ -93,14 +102,58 @@ console.log(basket);
 
 // TESTING isFull function (for false)
 
-console.log(isFull()); // Statement output: true
+console.log('Is the baseket currently full?', isFull()); // Statement output: false
 
 // Adding items to basket array
+
+console.log('What is in the basket?', basket);
 addItem('fruit');
 addItem('snacks');
-addItem('bread');
-console.log(basket);
+console.log('What is in the basket?',basket);
 
 // TESTING isFull function (for true)
 
-console.log(isFull()); // Statement output: false
+console.log('Is the basket currently full?', isFull()); // Statement output: true
+
+
+
+// 3. Update the required addItem function to:
+  // Use the isFull function to prevent more than maxItems from being added to the basket.
+  // If an item was added to the array, return true.
+  // If there was no room and the item could not be added return false.
+
+// SEE ABOVE (LINE 17) FOR REVISIONS MADE TO STRETCH GOAL #3 //
+  //NOTE: const 'maxItems' was created on line 5 for proprer initialization within addItem function.
+
+// Emptying basket to test update made to addItem function
+
+empty();
+console.log('Clearing the basket', basket); //Statement output: basket it empty ([])
+
+// Adding items to the basket to test update made to addItem function
+
+console.log('Adding item to basket', addItem('cheese')); // Statement output: TRUE, because basket is not full
+console.log('What is in the basket?', basket); // Statement output: ['cheese']
+
+console.log('Adding item to basket', addItem('bread')); // Statement output: TRUE, because basket is not full
+console.log('What is in the basket?', basket); // Statement output: ['cheese', 'bread']
+
+console.log('Adding item to basket', addItem('carrots')); // Statement output: TRUE, because basket is not full
+console.log('What is in the basket?', basket); // Statement output: ['cheese', 'bread', 'carrots']
+
+console.log('Adding item to basket', addItem('bacon')); // Statement output: TRUE, because basket is not full
+console.log('What is in the basket?', basket); // Statement output: ['cheese', 'bread', 'carrots', 'bacon']
+
+console.log('Adding item to basket', addItem('candy')); // Statement output: TRUE, because basket is not full
+console.log('What is in the basket?', basket); // Statement output: ['cheese', 'bread', 'carrots', 'bacon', 'candy']
+
+console.log('Adding item to basket', addItem('coffee')); // Statement output: FALSE, because basket is full
+console.log('What is in the basket?', basket); // Statement output: ['cheese', 'bread', 'carrots', 'bacon', 'candy']
+
+
+
+// 4. Create a function called removeItem. It should:
+  // Take an input parameter for a string item
+  // Use Array.indexOf to find the index of the first matching item in the basket
+  // Use Array.splice to remove the first matching item from the basket
+  // Return the item removed or 'null if the item was not found'
