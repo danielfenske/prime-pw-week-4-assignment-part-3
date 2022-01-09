9eaa42175a34015aa4435754759620ea3df4dee7
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-01-08 22:20:04 -0600

    Created and tested basket array

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,8 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+// 1. Create a global variable named 'basket' and set it to an empty array.
+
+let basket = [];
+console.log(basket); //Statement output: [] (empty array)
