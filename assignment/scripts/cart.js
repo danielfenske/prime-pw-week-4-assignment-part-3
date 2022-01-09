0efcbee1e61157057df40cbded0ac06f5086e1ae
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-01-08 22:39:59 -0600

    Created and tested 'listItems' function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -7,6 +7,8 @@ console.log('***** Cart Functions *****');
 let basket = [];
 console.log(basket); //Statement output: [] (empty array)
 
+
+
 // 2. Create a function called addItem. It should:
   // Take an input paramter for a string 'item'
   // Add the new item to the global array 'basket'
@@ -18,8 +20,26 @@ function addItem(item) {
   return true; //verifies item was added to basket
 } // end addItem
 
+
 // TESTING basket function
-// addItem('bread');
-// console.log(basket);
-// addItem('berries');
-// console.log(basket); //Statement output: ['bread', 'berries']
+addItem('bread');
+console.log(basket);
+addItem('berries');
+console.log(basket); //Statement output: ['bread', 'berries']
+
+
+
+// 3. Create a function called 'listItems'. It should:
+  // Loop over the items in the basket array
+  // Console.log each individual item on a new line
+
+function listItems() {
+  console.log('in listItems');
+  for (let i=0; i<basket.length; i++) {
+    console.log(basket[i]);
+  }
+} // end listItems
+
+
+// TESTING basket function
+listItems(); // Statement output: items in basket logged on separate lines ('bread', 'berries')
